c4e6f3d5590c97974044e85e63d270aa6e2a7f78
daudi13 <davouma13@gmail.com>
2022-03-15 12:40:19 +0300

    Add HTML for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,29 @@
 function Calculator() {
   return (
     <div className="calc-grid">
-      <div className="output">0</div>
+      <div className="output">
+        <div className="pre-operand"> </div>
+        <div className="cur-operand">0</div>
+      </div>
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button">/</button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button">*</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button">-</button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">+</button>
+      <button type="button" className="span-two">0</button>
+      <button type="button">.</button>
+      <button type="button">=</button>
     </div>
   );
 }
