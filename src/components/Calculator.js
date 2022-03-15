7c4e9bcf3de350b18d4c1ce5f5ee9f72d454ce6e
daudi13 <davouma13@gmail.com>
2022-03-15 13:17:45 +0300

    Add styling for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 function Calculator() {
   return (
     <div className="calc-grid">
@@ -8,22 +10,22 @@ function Calculator() {
       <button type="button">AC</button>
       <button type="button">+/-</button>
       <button type="button">%</button>
-      <button type="button">/</button>
+      <button type="button" className="orange">/</button>
       <button type="button">7</button>
       <button type="button">8</button>
       <button type="button">9</button>
-      <button type="button">*</button>
+      <button type="button" className="orange">*</button>
       <button type="button">4</button>
       <button type="button">5</button>
       <button type="button">6</button>
-      <button type="button">-</button>
+      <button type="button" className="orange">-</button>
       <button type="button">1</button>
       <button type="button">2</button>
       <button type="button">3</button>
-      <button type="button">+</button>
+      <button type="button" className="orange">+</button>
       <button type="button" className="span-two">0</button>
       <button type="button">.</button>
-      <button type="button">=</button>
+      <button type="button" className="orange">=</button>
     </div>
   );
 }
