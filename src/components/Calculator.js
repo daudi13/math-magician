5806483ca40a0f40d0f5acdf5b98d00bef1815f2
daudi13 <davouma13@gmail.com>
2022-03-16 16:57:27 +0300

    Add doMathOperation to button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -33,31 +33,31 @@ class Calculator extends Component {
   }
 
   render() {
+    const { next, total } = this.state;
     return (
       <div className="calc-grid">
         <div className="output">
-          <div className="pre-operand"> </div>
-          <div className="cur-operand">0</div>
+          <div className="cur-operand">{ total && next ? next : total || next || 0 }</div>
         </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="orange">/</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="orange">*</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="orange">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="span-two">0</button>
-        <button type="button">.</button>
-        <button type="button" className="orange">=</button>
+        <button onClick={this.doMathOperations} type="button">AC</button>
+        <button onClick={this.doMathOperations} type="button">+/-</button>
+        <button onClick={this.doMathOperations} type="button">%</button>
+        <button onClick={this.doMathOperations} type="button" className="orange">/</button>
+        <button onClick={this.doMathOperations} type="button">7</button>
+        <button onClick={this.doMathOperations} type="button">8</button>
+        <button onClick={this.doMathOperations} type="button">9</button>
+        <button onClick={this.doMathOperations} type="button" className="orange">*</button>
+        <button onClick={this.doMathOperations} type="button">4</button>
+        <button onClick={this.doMathOperations} type="button">5</button>
+        <button onClick={this.doMathOperations} type="button">6</button>
+        <button onClick={this.doMathOperations} type="button" className="orange">-</button>
+        <button onClick={this.doMathOperations} type="button">1</button>
+        <button onClick={this.doMathOperations} type="button">2</button>
+        <button onClick={this.doMathOperations} type="button">3</button>
+        <button onClick={this.doMathOperations} type="button" className="orange">+</button>
+        <button onClick={this.doMathOperations} type="button" className="span-two">0</button>
+        <button onClick={this.doMathOperations} type="button">.</button>
+        <button onClick={this.doMathOperations} type="button" className="orange">=</button>
       </div>
     );
   }
