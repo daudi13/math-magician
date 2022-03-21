a1b7d83e29fdb002e55090a3785d9ae8591d2b93
daudi13 <davouma13@gmail.com>
2022-03-21 07:36:24 +0300

    Remove extra square brackets in the ButtonInfo.js file

--- src/components/ButtonInfo.js
+++ src/components/ButtonInfo.js
@@ -1,34 +1,24 @@
 function Buttons() {
   const ButtonData = [
-    [
-      { name: 'AC' },
-      { name: '+/-' },
-      { name: '%' },
-      { className: 'orange', name: '/' },
-    ],
-    [
-      { name: '7' },
-      { name: '8' },
-      { name: '9' },
-      { className: 'orange', name: 'x' },
-    ],
-    [
-      { name: '4' },
-      { name: '5' },
-      { name: '6' },
-      { className: 'orange', name: '-' },
-    ],
-    [
-      { name: '1' },
-      { name: '2' },
-      { name: '3' },
-      { className: 'orange', name: '+' },
-    ],
-    [
-      { className: 'span-two', name: '0' },
-      { name: '.' },
-      { className: 'orange', name: '=' },
-    ],
+    { name: 'AC' },
+    { name: '+/-' },
+    { name: '%' },
+    { className: 'orange', name: '/' },
+    { name: '7' },
+    { name: '8' },
+    { name: '9' },
+    { className: 'orange', name: 'x' },
+    { name: '4' },
+    { name: '5' },
+    { name: '6' },
+    { className: 'orange', name: '-' },
+    { name: '1' },
+    { name: '2' },
+    { name: '3' },
+    { className: 'orange', name: '+' },
+    { className: 'span-two', name: '0' },
+    { name: '.' },
+    { className: 'orange', name: '=' },
   ];
 
   return ButtonData;
