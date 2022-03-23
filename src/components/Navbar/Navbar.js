538e3f999d5afadf183231ff40fe8a8722893414
daudi13 <davouma13@gmail.com>
2022-03-23 13:06:16 +0300

    Add ul for navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => (
-  <nav className="navBar">
-    <Link to="/">Home</Link>
-    |
-    {' '}
-    <Link to="App">Calculator</Link>
-    |
-    {' '}
-    <Link to="Quotes">Quotes</Link>
-  </nav>
+  <div className="navBar">
+    <div className="logo">Math</div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="App">Calculator</Link></li>
+      <li><Link to="Quotes">Quotes</Link></li>
+    </ul>
+  </div>
 );
 
 export default Navbar;
