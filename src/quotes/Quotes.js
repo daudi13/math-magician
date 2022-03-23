0f5c29fd30cda7d5a0983dddb9dcb3b8480a3ad8
daudi13 <davouma13@gmail.com>
2022-03-23 11:26:37 +0300

    Add Quotes.js

---
+++ src/quotes/Quotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const quotes = () => (
+  <div className="quote-box">
+    <p>Pure mathematics is, in its way, the poetry of logical ideas.</p>
+    <p><strong>-Albert Einstein, German theoretical physicist</strong></p>
+  </div>
+);
+
+export default quotes;
