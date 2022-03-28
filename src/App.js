81e39dd0884258578b46148e70192faead1154c1
daudi13 <davouma13@gmail.com>
2022-03-28 16:44:57 +0300

    Add giphy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.PureComponent {
       <div className="box">
         <div>
           <h2 className="sub-two">Let us do some math</h2>
-          <div className="blackboard" />
+          <img src="https://media.giphy.com/media/MB75OzWrpUMOWfBHg0/giphy.gif" alt="gif" height="300" />
         </div>
         <Calculator />
       </div>
