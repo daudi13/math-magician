583b1c975504c1f8fc9d0a26e65f97fb326b7dc2
daudi13 <davouma13@gmail.com>
2022-03-28 17:33:20 +0300

    Add link class

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => (
   <div className="navBar">
     <div className="logo">MATH101</div>
     <ul className="nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="App">Calculator</Link></li>
-      <li><Link to="Quotes">Quotes</Link></li>
+      <li><NavLink className="link-btn" to="/">Home</NavLink></li>
+      <li><NavLink className="link-btn" to="App">Calculator</NavLink></li>
+      <li><NavLink className="link-btn" to="Quotes">Quotes</NavLink></li>
     </ul>
   </div>
 );
