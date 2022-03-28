4e3e4a17f7e4cae2700823bb61c30eeb0bba0185
daudi13 <davouma13@gmail.com>
2022-03-28 17:43:38 +0300

    Add active class

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,14 @@ import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div className="navBar">
+  <header>
     <div className="logo">MATH101</div>
     <ul className="nav">
       <li><NavLink className="link-btn" to="/">Home</NavLink></li>
       <li><NavLink className="link-btn" to="App">Calculator</NavLink></li>
       <li><NavLink className="link-btn" to="Quotes">Quotes</NavLink></li>
     </ul>
-  </div>
+  </header>
 );
 
 export default Navbar;
